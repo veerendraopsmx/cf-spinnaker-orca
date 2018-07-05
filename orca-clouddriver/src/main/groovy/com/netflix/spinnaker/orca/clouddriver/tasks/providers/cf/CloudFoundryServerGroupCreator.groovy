f415d38360b6ad71f048c9d6eb2851eed3debe97
Jon Schneider <jschneider@pivotal.io>
2018-07-05 15:35:18 -0500

    Enough for 'Create Server Group'

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/providers/cf/CloudFoundryServerGroupCreator.groovy
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/providers/cf/CloudFoundryServerGroupCreator.groovy
@@ -26,57 +26,19 @@ import org.springframework.stereotype.Component
 class CloudFoundryServerGroupCreator implements ServerGroupCreator {
 
   boolean katoResultExpected = false
-  String cloudProvider = "cf"
+  String cloudProvider = "cloudfoundry"
 
   @Override
   List<Map> getOperations(Stage stage) {
     def operation = [:]
-
-    if (stage.context.containsKey("cluster")) {
-      operation.putAll(stage.context.cluster as Map)
-    } else {
-      operation.putAll(stage.context)
-    }
-
-    if (operation.account && !operation.credentials) {
-      operation.credentials = operation.account
-    }
+    operation.putAll(stage.context)
 
     if (stage.execution.hasProperty('trigger')) {
       operation.trigger = stage.execution.trigger
+//      if(operation.triger instance )
     }
 
-    // If this is a stage in a pipeline, look in the context for the baked image.
-    def deploymentDetails = (stage.context.deploymentDetails ?: []) as List<Map>
-
-    if (!operation.image && deploymentDetails) {
-      // Bakery ops are keyed off cloudProviderType
-      operation.image = deploymentDetails.find { it.cloudProviderType == 'cf' }?.imageId
-
-      // Alternatively, FindImage ops distinguish between server groups deployed to different zones.
-      // This is partially because AWS images are only available regionally.
-      if (!operation.image && stage.context.zone) {
-        operation.image = deploymentDetails.find { it.zone == stage.context.zone }?.imageId
-      }
-
-      // Lastly, fall back to any image within deploymentDetails, so long as it's unambiguous.
-      if (!operation.image) {
-        if (deploymentDetails.size() != 1) {
-          throw new IllegalStateException("Ambiguous choice of deployment images found for deployment to " +
-                  "'${stage.context.zone}'. Images found from cluster in " +
-                  "${deploymentDetails.collect{it.zone}.join(",") } - " +
-                  "only 1 should be available.")
-        }
-        operation.image = deploymentDetails[0].imageId
-      }
-    }
-
-    if (!operation.image && !operation.repository && !operation.artifact) {
-      throw new IllegalStateException("Neither an image nor a repository/artifact could be found in ${stage.context.zone}.")
-    }
-
-
-    return [[(ServerGroupCreator.OPERATION): operation]]
+    return [[(OPERATION): operation]]
   }
 
   @Override
