07ad053b73ef60095e83c006e8e54899549f0e8c
Jammy Louie <jlouie@pivotal.io>
2018-07-09 14:41:31 -0400

    add missing spaceId field

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/providers/cf/CloudFoundryServerGroupCreator.groovy
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/providers/cf/CloudFoundryServerGroupCreator.groovy
@@ -35,6 +35,7 @@ class CloudFoundryServerGroupCreator implements ServerGroupCreator {
       application: stage.context.application,
       credentials: stage.context.account,
       manifest: stage.context.manifest,
+      spaceId: stage.context.spaceId
     ]
 
     stage.context.stack?.with { operation.stack = it }
